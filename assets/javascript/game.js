abc01eaddcb91ee2bebb8c0a75204e3019c47304
Eric J <ejohnston092@gmail.com>
2018-05-15 00:02:43 -0400

    New functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,35 +10,35 @@ var gameOver = false;
 // Characters
 
 var obiWan = {
-    name = "Obi-Wan",
-    healthPoints = 120,
-    baseAttack = 6,
-    attackPower = 9,
-    counterAP = 8
+    name: "Obi-Wan",
+    healthPoints: 120,
+    baseAttack: 6,
+    attackPower:  9,
+    counterAP: 8,
 }
 
 var quiGon = {
-    name = "Qui-Gon",
-    healthPoints = 130,
-    baseAttack = 8,
-    attackPower = 9,
-    counterAP = 9
+    name: "Qui-Gon",
+    healthPoints: 130,
+    baseAttack: 8,
+    attackPower: 9,
+    counterAP: 9
 }
 
 var darthMaul = {
-    name = "Darth Maul",
-    healthPoints = 140,
-    baseAttack = 8,
-    attackPower = 10,
-    counterAP = 10,
+    name: "Darth Maul",
+    healthPoints: 140,
+    baseAttack: 8,
+    attackPower: 10,
+    counterAP: 10,
 }
 
 var darthSidious = {
-    name = "Darth Sideous",
-    healthPoints = 160,
-    baseAttack = 9,
-    attackPower = 12,
-    counterAP = 12
+    name: "Darth Sideous",
+    healthPoints: 160,
+    baseAttack: 9,
+    attackPower: 12,
+    counterAP: 12
 }
 
 //
@@ -52,15 +52,70 @@ function playerSelect(charSelected) {
 }
 
 function defenderSelect(defenderSelect) {
-    defender.name = defenderSelect.name;
-    defender.healthPoints = defenderSelect.healthPoints;
-    defender.baseAttack = defenderSelect.baseAttack;
-    defender.attackPower = defenderSelect.attackPower;
-    defender.counterAP = defenderSelect.counterAP;
+    defender.name = enemySelected.name;
+    defender.healthPoints = enemySelected.healthPoints;
+    defender.baseAttack = enemySelected.baseAttack;
+    defender.attackPower = enemySelected.attackPower;
+    defender.counterAP = enemySelected.counterAP;
 }
 
 //
 
 function setEnemies(){
-    
+    $(".available-characters").removeClass("available-characters").addClass("enemies-available");
+    $(".enemies").append($(".enemies-available"));
 }
+
+//
+$(document).ready(function(){
+
+    $("#obiwan").on("click", function() {
+
+        if(charSelected == false){
+            playerSelect(obiWan);
+            charSelected = true;
+            console.log(player);
+            $("#obiwan").removeClass("available-characters").attr("your-character")
+            $("#your-character").append($(this))
+            setEnemies();
+        }
+
+    });
+
+    $("#quigon").on("click", function() {
+        
+        if(charSelected == false){
+            playerSelect(obiWan);
+            charSelected = true;
+            console.log(player);
+            $("#quigon").removeClass("available-characters").attr("your-character")
+            $("#your-character").append($(this))
+            setEnemies();
+        }
+    });
+
+    $("#maul").on("click", function() {
+        
+        if(charSelected == false){
+            playerSelect(obiWan);
+            charSelected = true;
+            console.log(player);
+            $("#maul").removeClass("available-characters").attr("your-character")
+            $("#your-character").append($(this))
+            setEnemies();
+        }
+    });
+
+    $("#sidious").on("click", function() {
+        
+        if(charSelected == false){
+            playerSelect(obiWan);
+            charSelected = true;
+            console.log(player);
+            $("#sidious").removeClass("available-characters").attr("your-character")
+            $("#your-character").append($(this))
+            setEnemies();
+        }
+    });
+
+});
