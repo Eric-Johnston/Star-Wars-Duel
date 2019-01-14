626933036099c080ba22be19df613c3b37890e6d
Eric J <ejohnston092@gmail.com>
2019-01-14 16:29:23 -0500

    Further refactoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 
 const init = function () {
 
+    let player = {};
+    let enemy = {};
     let characters = {
         obiWan: {
             name: "Obi-Wan",
@@ -8,7 +10,8 @@ const init = function () {
             baseAttack: 8,
             attackPower: 7,
             counterAP: 13,
-            playerChar: null
+            playerChar: null,
+            enemyChar: null
         },
         quiGon: {
             name: "Qui-Gon",
@@ -16,7 +19,8 @@ const init = function () {
             baseAttack: 9,
             attackPower: 8,
             counterAP: 14,
-            playerChar: null
+            playerChar: null,
+            enemyChar: null
         },
         darthMaul: {
             name: "Darth Maul",
@@ -24,7 +28,8 @@ const init = function () {
             baseAttack: 10,
             attackPower: 9,
             counterAP: 15,
-            playerChar: null
+            playerChar: null,
+            enemyChar: null
         },
         darthSidious: {
             name: "Darth Sidious",
@@ -32,14 +37,14 @@ const init = function () {
             baseAttack: 11,
             attackPower: 10,
             counterAP: 16,
-            playerChar: null
+            playerChar: null,
+            enemyChar: null
         }
     };
 
     $(".available-characters").children().on('click', function () {
 
         $(this).removeClass("char-avail");
-        //We create the charID variable so we can evaluate the ID of the DOM element in our switch statement
         let charID = $(this).attr("id");
         let moveChar = $(this).addClass("user-char").removeClass("char-avail");
         let enemyChars = $(this).parent().children(".char-avail").addClass("enemy-char").removeClass("char-avail");
@@ -49,45 +54,83 @@ const init = function () {
             switch (charID) {
                 case "obiwan":
                     $(".user-character").append(moveChar);
+                    player = characters.obiWan;
+                    player.playerChar = true;
                     $(".enemies").append(enemyChars)[i];
-                    break;
+                    return;
                 case "quigon":
                     $(".user-character").append(moveChar);
+                    player = characters.quiGon;
+                    player.playerChar = true;
                     $(".enemies").append(enemyChars)[i];
-                    break;
+                    return;
                 case "maul":
                     $(".user-character").append(moveChar);
+                    player = characters.darthMaul;
+                    player.playerChar = true;
                     $(".enemies").append(enemyChars)[i];
-                    break;
+                    return;
                 case "sidious":
                     $(".user-character").append(moveChar);
+                    player = characters.darthSidious;
+                    player.playerChar = true;
                     $(".enemies").append(enemyChars)[i];
+                    return;
+                default:
+                    console.log("Something went wrong");
+                    break;
+            };
+        };
+
+        $(".enemies").children().on('click', function () {
+
+            let charID = $(this).attr("id");
+            let defender = $(this).addClass("defender-char").removeClass("user-char enemies");
+
+            switch (charID) {
+                case "obiwan":
+                    $("#defender-char").append(defender);
+                    enemy = characters.obiWan;
+                    enemy.enemyChar = true;
+                    break;
+                case "quigon":
+                    $("#defender-char").append(defender);
+                    enemy = characters.quiGon;
+                    enemy.enemyChar = true;
+                    break;
+                case "maul":
+                    $("#defender-char").append(defender);
+                    enemy = characters.darthMaul;
+                    enemy.enemyChar = true;
+                    break;
+                case "sidious":
+                    $("#defender-char").append(defender);
+                    enemy = characters.darthSidious;
+                    enemy.enemyChar = true;
                     break;
                 default:
-                    console.log("There was an error");
+                    console.log("Something went wrong");
+                    break;
             };
+        });
 
-            $(".enemies").children().on('click', function () {
+        $("#attack").on('click', function () {
 
-                let charID = $(this).attr("id");
-                let defender = $(this).addClass("defender-char").removeClass("user-char");
-    
-                switch (charID) {
-                    case "obiwan":
-                        $("#defender-char").append(defender);
-                        break;
-                    case "quigon":
-                        $("#defender-char").append(defender);
-                        break;
-                    case "maul":
-                        $("#defender-char").append(defender);
-                        break;
-                    case "sidious":
-                        $("#defender-char").append(defender);
-                        break;
-                };
-            });
-        };
+            //Player attack
+            enemy.healthPoints = enemy.healthPoints - player.baseAttack;
+            //Enemy attack
+            player.healthPoints = player.healthPoints - enemy.baseAttack;
+            $(".defender-char").children(".characterhp").html(`Health: ${enemy.healthPoints}`);
+            $(".user-char").children(".characterhp").html(`Health: ${player.healthPoints}`);
+            $("#gametxt").html(`<p>You attacked ${enemy.name} for ${player.baseAttack} damage!<p>`);
+            //Player AP increase
+            player.baseAttack = player.baseAttack + player.attackPower;
+            
+            if(enemy.healthPoints <= 0) {
+                $("#defender-char").html("");
+            }
+
+        });
     });
 };
 
